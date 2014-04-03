9f0a6caaaa54e132ed6d07ed1cc4ec32cb187d70
Alexander Svyazin <guybrush@live.ru>
2014-04-03 23:43:22 +0600

    very rough (but working) version of MetadataTool

--- MetadataTool/Main.hs
+++ MetadataTool/Main.hs
@@ -1,4 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Kafka.Messages.MetadataRequest
+import Kafka.Messages.MetadataResponse
+import Kafka.Messages.Request
+import Kafka.Messages.Response
+
+import Control.Monad.IO.Class
+import Data.Binary.Get
+import Data.Binary.Put
+import Data.ByteString (ByteString)
+import Data.ByteString.Lazy (toStrict, fromStrict)
+import Data.Conduit
+import Data.Conduit.Network
+import Data.Maybe
+
+testClientId :: ByteString
+testClientId = "testClient"
+
+logC :: (MonadIO m, Show a) => Conduit a m a
+logC = awaitForever $ \x -> do
+  liftIO $ print x
+  yield x
+
 main :: IO ()
-main = undefined
+main = runTCPClient (clientSettings 9092 "localhost") $ \appData -> do
+  let rawRequests = sendRawRequests =$= (appSink appData) :: Consumer RawRequest IO ()
+      rawResponses = (appSource appData) =$= receiveRawResponses :: Producer IO RawResponse
+  (yield $ RawRequest MetadataRequestApiKey 0 0 testClientId $ toStrict $ runPut $ putMetadataRequest $ MetadataRequest ["test"])
+    $$ rawRequests
+  rawResponses $$ do
+    maybeRawResp <- await
+    liftIO $ print $ runGet getMetadataResponse $ fromStrict $ responseMessageBytes $ fromJust maybeRawResp
