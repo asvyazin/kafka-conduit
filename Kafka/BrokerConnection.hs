4e7141baf41b4ed5aefa8162343f8e627ec3b13f
Alexander Svyazin <guybrush@live.ru>
2014-04-06 16:02:12 +0600

    sendRequestMessage, build fix

--- Kafka/BrokerConnection.hs
+++ Kafka/BrokerConnection.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Kafka.BrokerConnection where
 
 import Kafka.Messages.ApiKey
+import Kafka.Messages.MetadataRequest
 import Kafka.Messages.MetadataResponse
+import Kafka.Messages.ProduceRequest
 import Kafka.Messages.ProduceResponse
+import Kafka.Messages.Request
+import qualified Kafka.Messages.Response as Resp (correlationId)
 import Kafka.Messages.Response
 
 import Control.Applicative
@@ -12,7 +18,18 @@ import Data.Conduit
 import Data.Int
 import qualified Data.Map as M
 import Data.Serialize.Get
+import Data.Serialize.Put
 
+data RequestMessage = MetadataRequestMessage MetadataRequest
+                    | ProduceRequestMessage ProduceRequest
+
+getApiKey :: RequestMessage -> ApiKey
+getApiKey (MetadataRequestMessage _) = MetadataRequestApiKey
+getApiKey (ProduceRequestMessage _) = ProduceRequestApiKey
+                      
+data ResponseMessage = MetadataResponseMessage MetadataResponse
+                     | ProduceResponseMessage ProduceResponse
+                       
 data WaitingRequestsStore = WaitingRequestsStore { currentCorrelationId :: Int32
                                                  , requests :: M.Map Int32 ApiKey } deriving (Eq, Show)
 
@@ -22,8 +39,9 @@ putWaitingRequest = undefined
 getWaitingRequest :: WaitingRequestsStore -> Int32 -> (ApiKey, WaitingRequestsStore)
 getWaitingRequest = undefined
 
-data ResponseMessage = MetadataResponseMessage MetadataResponse
-                     | ProduceResponseMessage ProduceResponse
+putRequestMessage :: RequestMessage -> Put
+putRequestMessage (MetadataRequestMessage m) = putMetadataRequest m
+putRequestMessage (ProduceRequestMessage m) = putProduceRequest m
 
 deserializeResponseMessage :: ByteString -> ApiKey -> Either String ResponseMessage
 deserializeResponseMessage bytes MetadataRequestApiKey = MetadataResponseMessage <$> runGet getMetadataResponse bytes
@@ -32,8 +50,16 @@ deserializeResponseMessage bytes ProduceRequestApiKey = ProduceResponseMessage <
 receiveResponseMessage :: Monad m => Conduit RawResponse (StateT WaitingRequestsStore m) ResponseMessage
 receiveResponseMessage = awaitForever $ \raw -> do
   w <- get
-  let (apiKey, newW) = getWaitingRequest w $ correlationId raw
+  let (key, newW) = getWaitingRequest w $ Resp.correlationId raw
   put newW
-  case deserializeResponseMessage (responseMessageBytes raw) apiKey of
+  case deserializeResponseMessage (responseMessageBytes raw) key of
     Left _ -> return ()
     Right message -> yield message
+
+sendRequestMessage :: Monad m => Conduit RequestMessage (StateT WaitingRequestsStore m) RawRequest
+sendRequestMessage = awaitForever $ \req -> do
+  w <- get
+  let key = getApiKey req
+  let (corrId, newW) = putWaitingRequest w key
+  put newW
+  yield $ RawRequest key 0 corrId "testClient" $ runPut $ putRequestMessage req
