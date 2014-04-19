8fd7f3ee0d2ca08d5c0b2bdbe67e833db0f5bf41
Alexander Svyazin <guybrush@live.ru>
2014-04-19 15:36:26 +0600

    fix MessageSet serialization/deserialization

--- Kafka/Messages/MessageSet.hs
+++ Kafka/Messages/MessageSet.hs
@@ -4,6 +4,7 @@ import Kafka.Messages.Utils
 
 import Control.Applicative
 import Data.ByteString
+import qualified Data.ByteString as B (length)
 import Data.Int
 import Data.Serialize.Get hiding (getBytes)
 import Data.Serialize.Put
@@ -11,10 +12,19 @@ import Data.Serialize.Put
 type MessageSet = [MessageSetItem]
 
 putMessageSet :: MessageSet -> Put
-putMessageSet = putArray putMessageSetItem
+putMessageSet = putBytes . runPut . mapM_ putMessageSetItem
 
 getMessageSet :: Get MessageSet
-getMessageSet = getArray getMessageSetItem
+getMessageSet = do
+  messageSetSize <- fromEnum <$> getInt32be
+  getMessageSetWithSize messageSetSize
+
+getMessageSetWithSize :: Int -> Get MessageSet
+getMessageSetWithSize size | size < 0 = error "Invalid messageSet size"
+                           | otherwise = do
+                             item <- getMessageSetItem
+                             items <- getMessageSetWithSize (size - sizeofMessageSetItem item)
+                             return (item : items)
 
 data MessageSetItem = MessageSetItem { messageOffset :: Int64
                                      , message :: Message } deriving (Eq, Show)
@@ -41,3 +51,18 @@ putMessage m = putInt32be (messageCrc m) >>
 
 getMessage :: Get Message
 getMessage = Message <$> getInt32be <*> getInt8 <*> getInt8 <*> getBytes <*> getBytes
+
+sizeofMessageSetItem :: MessageSetItem -> Int
+sizeofMessageSetItem m = sizeofInt64 + sizeofMessage (message m)
+
+sizeofMessage :: Message -> Int
+sizeofMessage m = 3 * sizeofInt32 + 2 * sizeofInt8 + B.length (messageKey m) + B.length (messageValue m)
+
+sizeofInt64 :: Int
+sizeofInt64 = 8
+
+sizeofInt32 :: Int
+sizeofInt32 = 4
+
+sizeofInt8 :: Int
+sizeofInt8 = 1
